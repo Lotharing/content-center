864614c905b02399ba74b7de80ad2e6603e522a1
lutong <809676295@qq.com>
2021-03-27 22:23:18 +0800

    content-center test

---
+++ src/test/java/top/lothar/contentcenter/ContentCenterApplicationTests.java
@@ -0,0 +1,14 @@
+package top.lothar.contentcenter;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
