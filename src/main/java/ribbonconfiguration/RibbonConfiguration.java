2e9e486155e606b595f85009b66c45015530cf38
lutong <809676295@qq.com>
2021-03-28 00:33:26 +0800

    docs: Ribbon避免父子上下文重叠介绍

--- src/main/java/ribbonconfiguration/RibbonConfiguration.java
+++ src/main/java/ribbonconfiguration/RibbonConfiguration.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * <h1></h1>
+ * <h1>不能在application启动类包下，为了避免父子上下文重合，这里是做细粒度的配置，避免了重合就可以让规则指定某个服务使用，而不是让所有服务使用</h1>
  *
  * @author LuTong.Zhao
  * @Date 2021/3/28 00:21
