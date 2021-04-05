6f938e745ba9f703c9015d18129799679a222efc
lutong <809676295@qq.com>
2021-04-05 14:45:06 +0800

    feat: spring-cloud-stream生产消息通过binder直接投递到rocket server上

--- src/main/java/top/lothar/contentcenter/controller/content/ShareController.java
+++ src/main/java/top/lothar/contentcenter/controller/content/ShareController.java
@@ -3,6 +3,9 @@ package top.lothar.contentcenter.controller.content;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,5 +39,17 @@ public class ShareController {
     @RequestMapping("/get")
     public List<ServiceInstance> getDiscovery(){
         return this.discoveryClient.getInstances("user-center");
+    }
+
+    @Autowired
+    private Source source;
+
+    @GetMapping("/stream")
+    public String Stream(){
+        this.source.output()
+                .send(MessageBuilder
+                .withPayload("消息体")
+                .build());
+        return "success";
     }
 }
